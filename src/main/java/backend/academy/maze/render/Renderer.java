7f276fbb62d6a6743743ad69cb669075ecda4fc9
vegaklin <ruslan.evn@mail.ru>
2024-10-11 22:36:37 +0300

    feat: add renderer interface

--- src/main/java/backend/academy/maze/render/Renderer.java
+++ src/main/java/backend/academy/maze/render/Renderer.java
@@ -1,4 +1,10 @@
 package backend.academy.maze.render;
 
-public class Renderer {
+import backend.academy.maze.game.Coordinate;
+import backend.academy.maze.game.Maze;
+import java.util.List;
+
+public interface Renderer {
+    String render(Maze maze);
+//    String render(Maze maze, List<Coordinate> path);
 }
