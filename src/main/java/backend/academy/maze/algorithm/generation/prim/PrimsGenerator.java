19c179657b2a72d668cfe820ecfaa4c970712bf2
vegaklin <ruslan.evn@mail.ru>
2024-10-17 22:10:13 +0300

    feat: add Prims algorithm for maze generation

--- src/main/java/backend/academy/maze/algorithm/generation/prim/PrimsGenerator.java
+++ src/main/java/backend/academy/maze/algorithm/generation/prim/PrimsGenerator.java
@@ -1,30 +1,17 @@
-package backend.academy.maze.algorithm.generation;
+package backend.academy.maze.algorithm.generation.prim;
 
-import backend.academy.maze.maze.Cell;
+import backend.academy.maze.algorithm.generation.Generator;
 import backend.academy.maze.maze.Maze;
-import backend.academy.maze.render.ConsoleRenderer;
-import backend.academy.maze.render.Renderer;
-
 import java.awt.Point;
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Random;
 
 public class PrimsGenerator implements Generator {
     private final Random random;
+
     private static final int START_ROW = 1;
     private static final int START_COL = 1;
-
-    private static class Edge {
-        Point from;
-        Point to;
-        int weight;
-
-        Edge(Point from, Point to, int weight) {
-            this.from = from;
-            this.to = to;
-            this.weight = weight;
-        }
-    }
-    Renderer renderer = new ConsoleRenderer();
 
     public PrimsGenerator() {
         random = new Random();
@@ -32,38 +19,67 @@ public class PrimsGenerator implements Generator {
 
     @Override
     public Maze generate(int height, int width) {
-        Maze maze = new Maze(height, width);
-        PriorityQueue<Edge> frontier = new PriorityQueue<>(Comparator.comparingInt(e -> e.weight));
+        int newHeight = validateMazeSize(height);
+        int newWidth = validateMazeSize(width);
 
-        maze.addPassageToGrid(START_ROW, START_COL);
-        addFrontier(maze, frontier, START_ROW, START_COL, height, width);
+        Maze maze = new Maze(newHeight, newWidth);
+        PriorityQueue<Edge> availableEdges = new PriorityQueue<>(Comparator.comparingInt(edge -> edge.weight));
 
-        // Основной цикл генерации лабиринта
-        while (!frontier.isEmpty()) {
-            Edge edge = frontier.poll();
-            if (maze.isWallInGrid(edge.to.x, edge.to.y)) {
-                connect(maze, edge.from, edge.to);
-                addFrontier(maze, frontier, edge.to.x, edge.to.y, height ,width);
-            }
+        algorithmImplementation(maze, availableEdges, height, width);
 
-            System.out.println(renderer.render(maze));
-        }
-
-        return maze;
+        return addStartEndForMaze(maze, newHeight, newWidth);
     }
 
-    private void addFrontier(Maze maze, PriorityQueue<Edge> frontier, int x, int y, int height, int width) {
-        if (x > 1 && maze.isWallInGrid(x - 2, y)) frontier.add(new Edge(new Point(x, y), new Point(x - 2, y), random.nextInt()));
-        if (x < width - 2 && maze.isWallInGrid(x + 2, y)) frontier.add(new Edge(new Point(x, y), new Point(x + 2, y), random.nextInt()));
-        if (y > 1 && maze.isWallInGrid(x, y - 2)) frontier.add(new Edge(new Point(x, y), new Point(x, y - 2), random.nextInt()));
-        if (y < height - 2 && maze.isWallInGrid(x, y + 2)) frontier.add(new Edge(new Point(x, y), new Point(x, y + 2), random.nextInt()));
+    private void algorithmImplementation(Maze maze, PriorityQueue<Edge> availableEdges, int height, int width) {
+        maze.addPassageToGrid(START_ROW, START_COL);
+        addEdge(maze, availableEdges, START_ROW, START_COL, height, width);
+
+        while (!availableEdges.isEmpty()) {
+            Edge edge = availableEdges.poll();
+            if (maze.isWallInGrid(edge.to.y, edge.to.x)) {
+                connect(maze, edge.from, edge.to);
+                addEdge(maze, availableEdges, edge.to.y, edge.to.x, height, width);
+            }
+        }
+
+    }
+
+    private void addEdge(Maze maze, PriorityQueue<Edge> availableEdges, int y, int x, int height, int width) {
+        if (x > 1 && maze.isWallInGrid(y, x - 2)) {
+            availableEdges.add(new Edge(new Point(x, y), new Point(x - 2, y), random.nextInt()));
+        }
+        if (x < width - 2 && maze.isWallInGrid(y, x + 2)) {
+            availableEdges.add(new Edge(new Point(x, y), new Point(x + 2, y), random.nextInt()));
+        }
+        if (y > 1 && maze.isWallInGrid(y - 2, x)) {
+            availableEdges.add(new Edge(new Point(x, y), new Point(x, y - 2), random.nextInt()));
+        }
+        if (y < height - 2 && maze.isWallInGrid(y + 2, x)) {
+            availableEdges.add(new Edge(new Point(x, y), new Point(x, y + 2), random.nextInt()));
+        }
     }
 
     private void connect(Maze maze, Point p1, Point p2) {
         int x = (p1.x + p2.x) / 2;
         int y = (p1.y + p2.y) / 2;
-        maze.addPassageToGrid(p1.x, p1.y);
-        maze.addPassageToGrid(x, y);
-        maze.addPassageToGrid(p2.x, p2.y);
+        maze.addPassageToGrid(p1.y, p1.x);
+        maze.addPassageToGrid(y, x);
+        maze.addPassageToGrid(p2.y, p2.x);
+    }
+
+    private int validateMazeSize(int size) {
+        if (size <= 1) {
+            throw new IllegalArgumentException("Incorrect maze size.");
+        }
+        if (size % 2 == 0) {
+            return size + 1;
+        }
+        return size;
+    }
+
+    private Maze addStartEndForMaze(Maze maze, int height, int width) {
+        maze.addPassageToGrid(1, 0);
+        maze.addPassageToGrid(height - 2, width - 1);
+        return maze;
     }
 }
